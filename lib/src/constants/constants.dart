c6b4c441973385fa108fd3e11939e13b1d31b348
shan-shaji <abrahamshanshaji@gmail.com>
2025-06-19 15:34:51 +0200

    fix: dart formatting issue in contants.dart

--- lib/src/constants/constants.dart
+++ lib/src/constants/constants.dart
@@ -1,4 +1,4 @@
 const esc = '\u001B['; // Escape sequence introducer
 const osc = '\u001B]'; // Operating system command introducer
-const bel = '\u0007';  // Bell character (alert)
-const sep = ';';       // Separator for parameters
+const bel = '\u0007'; // Bell character (alert)
+const sep = ';'; // Separator for parameters
